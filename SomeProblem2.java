8c4c4df54d11cdc1a19d2bfa02d638bf462a05e3
saptadweepa <66834057+saptadweepa@users.noreply.github.com>
2024-11-04 23:45:55 +0100

    Create SomeProblem2.java

---
+++ SomeProblem2.java
@@ -0,0 +1,43 @@
+package practice;
+
+import java.util.List;
+import java.util.Map;
+
+public class SomeProblem2 {
+
+    Map<String, String> rollNumbers = Map.of(
+            "sharma", "EC01",
+            "singh", "EC02",
+            "kumar", "EC03",
+            "verma", "EC04",
+            "gupta", "EC05",
+            "mehta", "EC06",
+            "patel", "EC07",
+            "chaddha", "EC08",
+            "salvi", "EC09",
+            "shah", "EC10"
+    );
+    
+    Map<String, String> grades = Map.of(
+            "EC01", "A",
+            "EC02", "B",
+            "EC03", "A",
+            "EC04", "D",
+            "EC05", "C",
+            "EC06", "B",
+            "EC07", "A",
+            "EC08", "C",
+            "EC09", "B",
+            "EC10", "D"
+    );
+
+    public static void main(String[] args) {
+        //Write a program to print grades of all the students
+        //Expected output:
+        /*
+        * sharma: A
+        * salve: B
+         */
+    }
+    
+}
